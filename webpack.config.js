30ecaee38a7faca0d75fb9ef62cb4710d0b2a91d
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-08-13 17:09:43 +0200

    change in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = (env) => {
         output: {
             path: path.resolve(__dirname, 'public'),
             filename: 'app.bundle.js',
+          }
         devServer: {
              proxy: {
                 '/socket.io': {
