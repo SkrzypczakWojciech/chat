6ac519b386579fa293d41b4d95a35edc6d5c3c56
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-08-13 16:02:50 +0200

    change in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,54 @@
 const path = require('path');
 
-    const plugins = [new HtmlWebpackPlugin({
-      template: 'client/index.html',
-      filename: 'index.html',
-      inject: 'body'
-  })];
+const plugins = [new HtmlWebpackPlugin({
+    template: 'client/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
 
 module.exports = (env) => {
     return {
         mode: env || 'production',
         entry: './client/index.js',
         output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-    {
-        test: /\.js$/,
-        loader: "babel-loader"
-    },
-    {
-        test: /\.css$/,
-        use: [
-            { loader: 'style-loader'},
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: true
+            path: path.resolve(__dirname, 'public'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [{
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [{
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    options: {
+                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
                     }
                 }
             ]
-        },
-        {
-        test: /\.js$/,
-        loader: "babel-loader",
-        options: {
-        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
         }
     }
-    ]
-}
-}
 };
 
+devServer: {
+    proxy: {
+        '/socket.io': {
+            target: 'http://localhost:3000',
+            ws: true
+        }
+    }
+}
